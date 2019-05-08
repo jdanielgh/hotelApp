122d559d91b05e80aca4e9165c1517af058919e5
Jesús Daniel Gómez Hernández <jesus.gomezher@gmail.com>
2019-05-08 16:02:54 -0500

    Se sube proyecto compilado

---
+++ proyect/precache-manifest.18a51b4cbd6d426f57076bc8cbbd2eeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077b2a8fbe3428f86779a715ebd658cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9fa783a8aeb68b51810",
+    "url": "/static/css/2.6b854f14.chunk.css"
+  },
+  {
+    "revision": "d9fa783a8aeb68b51810",
+    "url": "/static/js/2.1569a550.chunk.js"
+  },
+  {
+    "revision": "238de57a06fcca74fc93",
+    "url": "/static/js/main.e26d5d96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
